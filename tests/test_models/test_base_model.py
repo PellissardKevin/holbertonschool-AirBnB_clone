e40c300816368799a66a50896b1ddc9ea6811f43
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-31 13:38:37 +0100

    moved folder test outside model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Unittest for class Base
+"""
+import unittest
+
+
+class test_basemodel(unittest.TestCase):
+    def test_int(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
