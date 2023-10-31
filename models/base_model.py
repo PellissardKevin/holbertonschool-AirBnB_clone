4beaacc564375a988d4274f8f5683221ce0dc387
Kevin.P <kevin.pellissard@gmail.com>
2023-10-31 14:04:21 +0100

    Add init with kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,18 @@ from datetime import datetime
 class BaseModel:
     """Definition of class basemodel"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        for key, value in kwargs.items():
+            if key == "created_at" or key == "updated_at":
+                value = datetime.now()
+            elif key == "__class__":
+                pass
+            else:
+                setattr(self, key, value)
 
     def __str__(self):
         """return string representation of the object"""
