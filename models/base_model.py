b451980214ceca846ef9eb8d437593fa22fe340f
ErwanR-Holberton <6218@holbertonstudents.com>
2023-10-31 12:42:55 +0100

    Added comments to basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+"""Definition of class basemodel"""
 import uuid
 import datetime
-"""Definition of class basemodel"""
 
 
 class BaseModel:
@@ -12,12 +12,15 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self):
+        """return string representation of the object"""
         return "[{}] ({}) {}".format(self.__class__.__name__,
                                      self.id, self.__dict__)
 
     def save(self):
+        """update the time"""
         self.updated_at = datetime.datetime.now().isoformat(sep='T')
 
     def to_dict(self):
+        """returns list of attributes including class name"""
         self.__dict__["__class__"] = self.__class__.__name__
         return self.__dict__
