dfcadcaf4c8591e790d0717987998224d416d9a9
Kevin.P <kevin.pellissard@gmail.com>
2023-10-31 13:21:14 +0100

    Update datetime with now methode and create init python file

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,12 @@ from datetime import datetime
 
 class BaseModel:
     """Definition of class basemodel"""
+
     def __init__(self):
+        """Initialize a new BaseModel."""
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """return string representation of the object"""
@@ -18,11 +20,12 @@ class BaseModel:
 
     def save(self):
         """update the time"""
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns list of attributes including class name"""
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        return self.__dict__
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy
