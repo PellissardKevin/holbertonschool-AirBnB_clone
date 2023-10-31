b005b503dc2deee08b6e9e8c9c2a7433c7445c80
Kevin.P <kevin.pellissard@gmail.com>
2023-10-31 13:06:10 +0100

    Update base model format datetime and dict format

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """Definition of class basemodel"""
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
     """Definition of class basemodel"""
     def __init__(self):
-        self.id = str(id(uuid.uuid4()))
-        self.created_at = datetime.datetime.now().isoformat(sep='T')
-        self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
     def __str__(self):
         """return string representation of the object"""
@@ -18,9 +18,11 @@ class BaseModel:
 
     def save(self):
         """update the time"""
-        self.updated_at = datetime.datetime.now().isoformat(sep='T')
+        self.updated_at = datetime.today()
 
     def to_dict(self):
         """returns list of attributes including class name"""
         self.__dict__["__class__"] = self.__class__.__name__
+        self.__dict__["created_at"] = self.created_at.isoformat()
+        self.__dict__["updated_at"] = self.updated_at.isoformat()
         return self.__dict__
