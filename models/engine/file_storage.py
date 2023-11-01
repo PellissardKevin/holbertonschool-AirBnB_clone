4864866366825cfb4608155c1c73d9bd48f31b61
Kevin.P <kevin.pellissard@gmail.com>
2023-11-01 23:13:11 +0100

    Update file storage Reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,25 +9,29 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """Returns a dictionaty '__objects'"""
         return self.__objects
 
     def new(self, obj):
+        """Sets in '__objects' the object with the key """
         index = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[index] = obj
 
     def save(self):
+        """Serializes '__objects' dictionary to a JSON file"""
         dict_to_save = {}
         for key, obj in self.__objects.items():
             dict_to_save[key] = obj.to_dict()
-
         with open(self.__file_path, "w") as file:
             json.dump(dict_to_save, file)
 
     def reload(self):
+        """Deserializes the JSON file to __objects' dictionary'"""
+        from models.base_model import BaseModel
         try:
-            with open(self.__file_path, "r") as file:
-                my_obj_dict = json.load(file)
-                for key, dict in my_obj_dict.items():
-                    self.__objects[key] = eval(dict['__class__'])(**dict)
-        except Exception as e:
+            with open(self.__file_path, encoding='utf-8') as File:
+                cls = '__class__'
+                for key, value in json.load(File).items():
+                    self.__objects[key] = eval(value[cls] + '(**value)')
+        except FileNotFoundError:
             pass
