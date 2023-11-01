5e406dc7663457897bfc1f19495b1a30dddac49c
ErwanR-Holberton <6218@holbertonstudents.com>
2023-11-01 16:43:54 +0100

    Currently editing file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,18 +13,19 @@ class FileStorage:
 
     def new(self, obj):
         index = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[index] = obj.to_dict()
+        self.__objects[index] = obj
 
     def save(self):
-        dict_to_json = {}
-        for key, value in self.__objects.items():
-            dict_to_json[key] = value
         with open(self.__file_path, "w") as file:
-            json.dump(dict_to_json, file)
+            for key, value in self.__objects.items():
+                print()
+                json.dump(value.to_dict(), file)
 
     def reload(self):
         try:
             with open(self.__file_path, "r") as file:
-                self.__objects = json.loads(file.read())
-        except Exception:
+                obj = BaseModel(json.load(file))
+                self.new(obj)
+        except Exception as e:
+            print(e)
             pass
