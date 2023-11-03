81713dcf6ca524296e4d0945db9e0ea2be65fa45
Kevin.P <kevin.pellissard@gmail.com>
2023-11-03 12:06:29 +0100

    add file for test classes

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Unittest for class Base"""
+import unittest
+import os
+from models.amenity import Amenity
+from datetime import datetime
+
+
+class test_amenity(unittest.TestCase):
+    """Test cases for Amenity class"""
